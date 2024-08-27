0303cfe7b81eed62108db983020aa4d2290128d0
caiosousa2003 <caiosousa080403@gmail.com>
2024-08-27 00:42:28 -0300

    feat: ForgotAndRedefinePasswordController

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,10 +1,9 @@
 import UserModel from "../Models/UserModel.js";
-import jwt from "jsonwebtoken";
-import { NotFoundError } from "../Errors/baseErrors.js";
-import { ForbiddenError } from "../Errors/baseErrors.js";
+import UserPwdTokenModel from "../Models/UserPwdTokenModel.js";
+import { signForgotPasswordJwt } from "../Utils/general/jwt.js";
+import * as EmailHandler from "../Utils/mail/handlers.js"
 
 class UserController {
-  async Login(req, res) {}
 
   async Create(req, res) {
     try {
@@ -26,6 +25,7 @@ class UserController {
       next(new NotFoundError(`Route '${req.baseUrl}' not found`));
     }
   }
+
   async Update(req, res) {
     try {
       const { id } = req.params;
@@ -36,6 +36,7 @@ class UserController {
       next(new ForbiddenError(`Route '${req.baseUrl}' forbidden`));
     }
   }
+
   async Destroy(req, res) {
     try {
       const { id } = req.params;
@@ -48,8 +49,65 @@ class UserController {
     }
   }
 
-  
+  async ForgotPassword(req, res) {
+    try {
+      const { email } = req.body;
+
+      const foundUser = await UserModel.findOne({ email, });
+      if (!foundUser) 
+        return res.status(404).json({ message: "Usuário não encontrado" });
+
+      const passwordToken = signForgotPasswordJwt(foundUser._id);
+
+      await UserPwdTokenModel.deleteMany({ user: foundUser._id }).exec();
+      await UserPwdTokenModel.create({
+        user: foundUser._id,
+        token: passwordToken,
+      });
+
+      await EmailHandler.redefinePasswordEmail({
+        user: foundUser,
+        passwordToken,
+      });
+
+      return res.status(200).json({ mensagem: "E-mail de recuperação enviado com sucesso!" });
+
+    } catch (error) {
+      res.status(500).json({ message: "ERRO!", error: error.message });
+    }
+  }
+
+  async RedifinePassword(req, res){
+    try {
+      const { newPassword } = req.body;
+      const { token } = req.params;
+
+      const { userId } = await decodeForgotPasswordToken(token);
+
+      const [foundUser, foundToken] = await Promise.all([
+        UserModel.findById(userId).exec(),
+        UserPwdTokenModel.findOne({ token }).exec(),
+      ]);
+
+      if (!foundUser) 
+        return res.status(404).json({ message: "Usuário não encontrado" });
+      if (!foundToken) 
+        return res.status(403).json({ message: "Token expirado ou não encontrado" });
+      if (userId !== foundToken.user.toString())
+        return res.status(403).json({ message: "Token adulterado" });
+
+      await foundToken.deleteOne();
+
+      const updatedUser = foundUser.set({ password: newPassword }).save();
+      
+      return res.status(200).json({ mensagem: "Senha alterada com sucesso!" });
+
+    } catch (error) {
+      res.status(500).json({ message: "ERRO!", error: error.message });
+    }
+  }
 
 }
+  
 
 export default new UserController();
