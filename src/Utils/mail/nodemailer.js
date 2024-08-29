e66a25ddfc6c45640ec3232a76c1d58f40a392da
caiosousa2003 <caiosousa080403@gmail.com>
2024-08-29 12:28:22 -0300

    fix: putUniqueTransporterConfig

--- src/Utils/mail/nodemailer.js
+++ src/Utils/mail/nodemailer.js
@@ -1,15 +1,6 @@
 import nodemailer from 'nodemailer';
 
 const transporterConfig = {
-  development: {
-    host: 'localhost',
-    port: 1025,
-    secure: false,
-    tls: {
-      rejectUnauthorized: false,
-    },
-  },
-  production: {
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     secure: false,
@@ -17,13 +8,9 @@ const transporterConfig = {
       user: process.env.EMAIL_FROM,
       pass: process.env.EMAIL_PASSWORD,
     },
-  },
 };
 
-const transporter = nodemailer.createTransport(
-  // transporterConfig[process.env.NODE_ENV]
-  transporterConfig['development']
-);
+const transporter = nodemailer.createTransport(transporterConfig);
 
 export default async function sendEmail(request) {
   const config = {
