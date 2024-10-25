c4d872c602f1ff67085eec02c07c0f7a80672696
Gustavo Rezende <gustavo.olicardoso07@gmail.com>
2024-10-25 10:25:05 -0300

    feat: VideoController + VideoVallidator

--- src/Controllers/VideoController.js
+++ src/Controllers/VideoController.js
@@ -1,33 +1,34 @@
 import VideosModel from "../Models/VideosModel.js";
-import ffmpeg from 'fluent-ffmpeg';
-import streamifier from 'streamifier';
-import { PassThrough } from 'stream';
-import { openai } from "../Config/OpenAI.js";
+import ffmpeg from "fluent-ffmpeg";
+import streamifier from "streamifier";
+import { PassThrough } from "stream";
+//import { openai } from "../Config/OpenAI.js";
 
 class VideosController {
   async Create(req, res) {
     try {
-    // objeto que chega { title, description, videoFile, code, context, responsible, totalParticipants, country, language, duration, date } - falta tirar videoFile e gerar thumbnail e transcription   
+      // objeto que chega { title, description, videoFile, code, context, responsible, totalParticipants, country, language, duration, date } - falta tirar videoFile e gerar thumbnail e transcription
       const { language, videoFile, code } = req.body;
 
-      const foundCode = await VideosModel.findOne({ code, });
+      const foundCode = await VideosModel.findOne({ code });
       if (foundCode) {
         return res.status(409).json({ message: "Código já cadastrado!" });
       }
 
-      // Conversão do arquivo para stream de memória
-      const videoStream = streamifier.createReadStream(Buffer.from(videoFile, 'base64'));
+      // Verificar se o arquivo foi enviado
+      const videoStream = streamifier.createReadStream(Buffer.from(videoFile, "base64"));
 
-      if (!videoStream) {
-        return res.status(409).json({ message: "Erro ao fazer a conversão de arquivo!" });
+      if (!videoFile) {
+        return res.status(409).json({ message: "Arquivo de vídeo não fornecido!" });
       }
 
       const thumbnailStream = new PassThrough();
 
-      ffmpeg(videoStream).screenshots({
-        timestamps: [5],
-      })
-      .pipe(thumbnailStream, { end: true }); //.on('error', (err) => {return res.status....})
+      ffmpeg(videoStream)
+        .screenshots({
+          timestamps: [5],
+        })
+        .pipe(thumbnailStream, { end: true }); //.on('error', (err) => {return res.status....})
 
       const chunks = [];
       for await (const chunk of thumbnailStream) {
@@ -35,7 +36,7 @@ class VideosController {
       }
       const buffer = Buffer.concat(chunks);
 
-      const thumbnailFile = buffer.toString('base64');
+      const thumbnailFile = buffer.toString("base64");
 
       if (!thumbnailFile) {
         return res.status(409).json({ message: "Erro ao gerar a thumbnail!" });
@@ -43,22 +44,22 @@ class VideosController {
 
       const audioStream = new PassThrough();
 
-      ffmpeg(videoStream).noVideo().format('wav').pipe(audioStream, { end: true }); //.on('error', (err) => {return res.status....})
+      ffmpeg(videoStream).noVideo().format("wav").pipe(audioStream, { end: true }); //.on('error', (err) => {return res.status....})
 
-      const transciption = await openai.audio.transcriptions.create({
-        file: audioStream,
-        model: 'whisper-1',
-        language: language, // Idioma principal do vídeo. Fornecer esse tipo de informação é opcional, entretanto auxilia a IA e traz maior eficiência
-        response_format: 'text',
-        temperature: 0, // nível de acertividade (valores mais altos possibilitam possíveis erros) 
-      })
+      // const transcription = await openai.audio.transcriptions.create({
+      //   file: audioStream,
+      //   model: 'whisper-1',
+      //   language: language, // Idioma principal do vídeo. Fornecer esse tipo de informação é opcional, entretanto auxilia a IA e traz maior eficiência
+      //   response_format: 'text',
+      //   temperature: 0, // nível de acertividade (valores mais altos possibilitam possíveis erros)
+      // })
 
-      if (!transciption) {
+      if (!transcription) {
         return res.status(409).json({ message: "Erro ao gerar a transcrição!" });
       }
 
-      const newVideo = { ...req.body, thumbnail: thumbnailFile, transciption: transciption };
-      
+      const newVideo = { ...req.body, thumbnail: thumbnailFile, transcription: transcription };
+
       const video = await VideosModel.create(newVideo);
 
       return res.status(200).json(video);
@@ -67,11 +68,19 @@ class VideosController {
     }
   }
 
-
-
   async GetVideo(req, res) {
     try {
-      const video = await VideosModel.find();
+      const { id } = req.params;
+      const video = await VideosModel.findById(id);
+      return res.status(200).json(video);
+    } catch (error) {
+      res.status(500).json({ message: "Not found", error: error.message });
+    }
+  }
+  async UpdateVideo(req, res) {
+    try {
+      const { id } = req.params;
+      const video = await VideosModel.findByIdAndUpdate(id, req.body, { new: true });
       return res.status(200).json(video);
     } catch (error) {
       res.status(500).json({ message: "Not found", error: error.message });
--- src/Validators/VideoValidator.js
+++ src/Validators/VideoValidator.js
@@ -3,43 +3,61 @@ import { z } from "zod";
 import { validateRequest } from "zod-express-middleware";
 
 const create = validateRequest({
-    body: z.object({
-    	title: z.string({ required_error: "O titulo é obrigatório" }),
-        description: z.
-            string({ required_error: "A descrição é obrigatória" })
-            .min(2, { message: "Description must be at least 2 characters long" })
-            .max(750, { message: "Description cannot exceed 750 characters" }),
-        video: z.string({ required_error: "O video é obrigatório" }),
-        code: z.string({ required_error: "O código é obrigatório" }),
-        context: z.
-            string({ required_error: "O contexto é obrigatória" })
-            .min(2, { message: "Context must be at least 2 characters long" })
-            .max(100, { message: "Context cannot exceed 100 characters" }),
-        responsible: z.string({ required_error: "Os responsáveis são obrigatórios" }),
-        totalParticipants: z.number({ required_error: "O número total de participantes é obrigatório" }),
-        country: z.string({ required_error: "O país de origem é obrigatório" }),
-        language: z.string({ required_error: "O idioma falado é obrigatório" }),
-        duration: z.number({ required_error: "A duração é obrigatória" }),
-        date: z.string({ required_error: "A data é obrigatória" }),
+  body: z.object({
+    title: z.string({ required_error: "O titulo é obrigatório" }),
+    description: z
+      .string({ required_error: "A descrição é obrigatória" })
+      .min(2, { message: "Description must be at least 2 characters long" })
+      .max(750, { message: "Description cannot exceed 750 characters" }),
+    video: z.string({ required_error: "O video é obrigatório" }),
+    code: z.string({ required_error: "O código é obrigatório" }),
+    context: z
+      .string({ required_error: "O contexto é obrigatória" })
+      .min(2, { message: "Context must be at least 2 characters long" })
+      .max(100, { message: "Context cannot exceed 100 characters" }),
+    responsible: z.string({ required_error: "Os responsáveis são obrigatórios" }),
+    totalParticipants: z.number({
+      required_error: "O número total de participantes é obrigatório",
     }),
+    country: z.string({ required_error: "O país de origem é obrigatório" }),
+    language: z.string({ required_error: "O idioma falado é obrigatório" }),
+    duration: z.number({ required_error: "A duração é obrigatória" }),
+    date: z.string({ required_error: "A data é obrigatória" }),
+  }),
 });
 
 const get = validateRequest({
-    params: z.object({
-        id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
-    }),
-
+  params: z.object({
+    id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
+    title: z.string().optional(),
+    description: z.string().optional(),
+    video: z.string().optional(),
+    responsble: z.string().optional(),
+    totalParticipants: z.number().optional(),
+    country: z.string().optional(),
+    language: z.string().optional(),
+    duration: z.number().optional(),
+    date: z.string().optional(),
+  }),
+});
+const update = validateRequest({
+  params: z.object({
+    id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
+    title: z.string().optional(),
+    description: z.string().optional(),
+    //transcription: z.string().optional(),
+  }),
 });
 
 const destroy = validateRequest({
-    params: z.object({
-        id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
-    }),
-
+  params: z.object({
+    id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
+  }),
 });
 
 export default {
-    create,
-    get,
-    destroy,
+  create,
+  get,
+  update,
+  destroy,
 };
