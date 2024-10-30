17c7b0baa2cfe938634162e49e545fe56d8fdde1
Gustavo Rezende <gustavo.olicardoso07@gmail.com>
2024-10-30 16:26:13 -0300

    fix: gramatical errors

--- src/Models/VideosModel.js
+++ src/Models/VideosModel.js
@@ -2,75 +2,87 @@ import mongoose, { mongo } from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const VideosSchema = new Schema({
+const VideosSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
-    description: {
-        type: String,
-        required: true,
-        trim: true,
+    shortDescription: {
+      type: String,
+      required: true,
+      trim: true,
     },
+
     video: {
-        type: String,
-        required: true,
-        enum: ['video/x-flv', 'video/mp4', 'video/MP2T', 'video/3gpp', 'video/quicktime', 'video/x-msvideo', 'video/x-ms-wmv',],
-        unique: true,
+      type: String,
+      required: true,
+      enum: [
+        "video/x-flv",
+        "video/mp4",
+        "video/MP2T",
+        "video/3gpp",
+        "video/quicktime",
+        "video/x-msvideo",
+        "video/x-ms-wmv",
+      ],
+      unique: true,
     },
     thumbail: {
-        type: String,
-        required: true,
-        enum: ['image/jpeg', 'image/png', 'image/gif', 'image/bmp', 'image/webp', 'image/svg+xml'],
+      type: String,
+      required: true,
+      enum: ["image/jpeg", "image/png", "image/gif", "image/bmp", "image/webp", "image/svg+xml"],
     },
     code: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
     context: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
-    responsible: {
-        type: String,
-        required: true,
-        trim: true,
+    responsibles: {
+      type: String,
+      required: true,
+      trim: true,
     },
     totalParticipants: {
-        type: Number,
-        required: true,
-        trim: true,
+      type: Number,
+      required: true,
+      trim: true,
     },
     country: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     language: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     duration: {
-        type: Number,
-        min: [0, 'Video duration cannot be less than 0 seconds'],
+      type: Number,
+      min: [0, "Video duration cannot be less than 0 seconds"],
     },
     date: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     transcription: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
-},{
-    timestamps: true
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const VideosModel = mongoose.model("videos", VideosSchema);
 
